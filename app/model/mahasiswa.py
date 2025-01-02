4e4417476cce9fd05b674cd9c7c05006fdff1c0c
Nathan Abraham Sinaga <123798742+NathanAbrahamSinaga@users.noreply.github.com>
2025-01-02 09:04:23 +0700

    Update mahasiswa.py

--- app/model/mahasiswa.py
+++ app/model/mahasiswa.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Mahasiswa(db.Model):
-    npm = db.Column(db.Integer, primary_key=True)
+    npm = db.Column(db.String(80), primary_key=True)
     nama = db.Column(db.String(80), nullable=False)
     tgl_lahir = db.Column(db.DateTime, nullable=False)
     alamat = db.Column(db.String(80), nullable=False)
